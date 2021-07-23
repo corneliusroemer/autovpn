0dc9a4eb3ec11b75c55eb6e7f968014aaa1bc877
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-07-24 00:59:16 +0200

    Update README and setup.py

--- README.md
+++ README.md
@@ -2,7 +2,38 @@
 
 ## Installation
 
+```
+pip install autovpn==0.1.0
+```
 
 ## Usage
 
+```
+Usage: autovpn [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  configure   Configure VPN
+  connect     Connect to VPN
+  disconnect  Disconnect from VPN
+  state       Show VPN state
+```
+
 ## Configuration
+
+You need to add a configuration file `~/.vpnconfig` to your home directory with the following content:
+```
+# Path to Cisco Anyconnect VPN binary
+vpnpath = "/opt/cisco/anyconnect/bin/vpn" 
+
+# Server to connect to
+server = "vpn.mobile.unibas.ch"
+
+# Username to connect with
+username = "USERNAME" 
+
+# Password to connect with
+password = "PASSWORD" 
+```
--- setup.py
+++ setup.py
@@ -1,20 +1,39 @@
 from setuptools import setup
 
 setup(
-    name='autovpn',
-    description='A CLI wrapper for Cisco\'s Anyconnect VPN tool',
-    license='MIT',
-    author='Cornelius Roemer',
-    version='0.1.0',
-    py_modules=['autovpn'],
+    name="autovpn",
+    description="A CLI wrapper for Cisco's Anyconnect VPN tool",
+    license="MIT",
+    author="Cornelius Roemer",
+    url="https://github.com/corneliusroemer/autovpn",
+    version="0.1.0",
+    py_modules=["autovpn"],
     install_requires=[
-        'Click',
-        'sortedcontainers',
-        'toml',
+        "Click",
+        "sortedcontainers",
+        "toml",
     ],
+    python_requires=">=3.6",
     entry_points={
-        'console_scripts': [
-            'autovpn = autovpn:cli',
+        "console_scripts": [
+            "autovpn = autovpn:cli",
         ],
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet",
+        "Topic :: System :: Networking",
+        "Topic :: Utilities",
+    ],
+    keywords="cli wrapper cisco anyconnect vpn passwordless",
+    project_urls={
+        "Documentation": "https://github.com/corneliusroemer/autovpn",
+        "Source": "https://github.com/corneliusroemer/autovpn",
+        "Tracker": "https://github.com/corneliusroemer/autovpn/issues",
     },
 )
