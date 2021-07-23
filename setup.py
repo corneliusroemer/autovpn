e2bd65f5015ce74e80ce1f43a62468d7ded46c1b
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-07-24 00:13:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name='autovpn',
+    description='A CLI wrapper for Cisco\'s Anyconnect VPN tool',
+    license='MIT',
+    author='Cornelius Roemer',
     version='0.1.0',
     py_modules=['autovpn'],
     install_requires=[
